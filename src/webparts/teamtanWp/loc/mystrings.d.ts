c9710f87310c35be8b274caa5ecbac1db689b1ae
“rakesh@w2learnms.onmicrosoft.com” <rakesh@w2learnms.onmicrosoft.com>
2024-11-03 23:46:35 +0530

    test

---
+++ src/webparts/teamtanWp/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITeamtanWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TeamtanWpWebPartStrings' {
+  const strings: ITeamtanWpWebPartStrings;
+  export = strings;
+}
